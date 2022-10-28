e0e26043124fb82c991623d425d31d9d3bb67038
Sergey Zarubin <sergey310872@yandex.ru>
2022-10-28 21:39:33 +0300

    12-th commit

--- HomeWork_ALTSHU/src/HomeWork_7/Task_7_2/Main.java
+++ HomeWork_ALTSHU/src/HomeWork_7/Task_7_2/Main.java
@@ -3,32 +3,33 @@ package HomeWork_7.Task_7_2;
 
 import java.util.Date;
 import java.util.Scanner;
+import java.util.concurrent.Semaphore;
 
 public class Main {
     static String currentItem = new String();
     static ThreadAgg threadAgg;
     static int choice;
-    static long timeFinish = new Date().getTime() + 10000;
+    static long timeFinish;
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        while (choice!=1 && choice!=2) {
+        while (choice != 1 && choice != 2) {
             System.out.print("Курица - Яйцо\n Введите значение (1 - курица, 2 - яйцо): ");
             choice = in.nextInt();
         }
         System.out.println("Игра начинается:");
-//        long timeStart = new Date().getTime();
-//        long timeFinish = timeStart + 10000;
+        timeFinish = new Date().getTime() + 500;
 
-        ThreadAgg tAgg1 = new ThreadAgg("Курица");
+        Semaphore sem = new Semaphore(1); // 1 разрешение
+        ThreadAgg tAgg1 = new ThreadAgg("Курица", sem);
+        tAgg1.setPriority(Thread.MAX_PRIORITY);
         tAgg1.start();
-        ThreadAgg tAgg2 = new ThreadAgg("       Яйцо");
+        ThreadAgg tAgg2 = new ThreadAgg("       Яйцо", sem);
+        tAgg2.setPriority(Thread.MIN_PRIORITY);
         tAgg2.start();
-        if (tAgg1.isAlive()) System.out.println("agg1");
-        if (tAgg2.isAlive()) System.out.println("agg2");
-        while ((tAgg1.isAlive()) || (tAgg2.isAlive())){
+
+        while ((tAgg1.isAlive()) || (tAgg2.isAlive())) {
         }
-        System.out.println("finish");
         String lastItem = (Main.choice == 1) ? "Курица" : "       Яйцо";
         if (!lastItem.equals(currentItem)) {
             System.out.println(lastItem);
@@ -37,21 +38,27 @@ public class Main {
 }
 
 class ThreadAgg extends Thread {
-//    static String currentItem = new String();
     private String item;
+    Semaphore sem;
 
-    ThreadAgg(String it) {
+    ThreadAgg(String it, Semaphore sem) {
         this.item = it;
+        this.sem = sem;
     }
-
 
     @Override
     public void run() {
         while (Main.timeFinish > new Date().getTime()) {
-            if ((!Main.currentItem.equals(item))) {
-                Main.currentItem = item;
-                System.out.println(Main.currentItem);
+            try {
+                sem.acquire();
+                if ((!Main.currentItem.equals(item))) {
+                    Main.currentItem = item;
+                    System.out.println(Main.currentItem);
+                }
+            } catch (InterruptedException e) {
+                System.out.println(e);
             }
+            sem.release();
         }
     }
 }
