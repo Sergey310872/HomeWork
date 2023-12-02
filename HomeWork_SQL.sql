f8f564bf1e3b18de3d7e1421526fef7302bb0a72
Sergey310872 <sergey310872@yandex.ru>
2023-12-02 11:10:40 +0300

    2023 commit

---
+++ HomeWork_SQL.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.personal(
+	id, name, salary, email, department_id, manager_id)
+	VALUES (1, 'Иванов.И.И.', 220000.50, null, 1, 0);
+INSERT INTO public.personal(
+	id, name, salary, email, department_id, manager_id)
+	VALUES (2, 'Гневашева Л.И.', 150000.20, null, 5, 1);
+INSERT INTO public.personal(
+	id, name, salary, email, department_id, manager_id)
+	VALUES (3, 'Гурковский С.Б.', 190000.00, null, 1, 1);
